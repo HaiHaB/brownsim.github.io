b57a80ee3c4a01e803dd5801db73ebfd15d8ca71
HaiHa <hai.ha.thi.do@gmail.com>
2024-08-15 16:29:30 +0700

    HD-0002: Added correct url for cookies.html and email address

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,49 @@
 (function(){
 
     new cookieNoticeJS({
-    
+
         // Localizations of the notice message
         messageLocales: {
           en: 'We use anonymous cookies on this website to monitor and improve upon site performance.<br/> By continuing to use our site you agree to this use. You can learn more and remove these cookies at any point via our'
         },
-       
+
         // Localizations of the dismiss button text
         buttonLocales: {
-          en: 'Ok'
+          en: 'I ACCEPT'
         },
-        
+
         // Position for the cookie-notifier (default=bottom)
         cookieNoticePosition:'bottom',
-        
+
         // Shows the "learn more button (default=false)
         learnMoreLinkEnabled: true,
-        
-        // The href of the learn more link must be applied if (learnMoreLinkEnabled=true) 
-        learnMoreLinkHref:'cookie.html',
-        
+
+        // The href of the learn more link must be applied if (learnMoreLinkEnabled=true)
+        learnMoreLinkHref:'https://app.termly.io/policy-viewer/policy.html?policyUUID=33799b21-62d2-4d4c-94d8-2627fb48e5e3',
+
         // Text for optional learn more button
         learnMoreLinkText:{
             en:'cookie page'
         },
-        
+
         // The message will be shown again in X days
-        expiresIn: 30, 
-        
+        expiresIn: 30,
+
         // Dismiss button background color
-        buttonBgColor: '#d35400',  
-        
+        buttonBgColor: '#d35400',
+
         // Dismiss button text color
-        buttonTextColor: '#fff', 
-          
+        buttonTextColor: '#fff',
+
         // Notice background color
-        noticeBgColor: '#000', 
-           
+        noticeBgColor: '#000',
+
         // Notice text color
-        noticeTextColor: '#fff', 
-        
-        // the lernMoreLink color (default='#009fdd') 
+        noticeTextColor: '#fff',
+
+        // the lernMoreLink color (default='#009fdd')
         linkColor:'#f00'
-           
+
      });
 
 })();
