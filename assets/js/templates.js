ec6293ca8d95cbbf72d86fe8f5bbeb07e933fe04
HaiHa <hai.ha.thi.do@gmail.com>
2025-09-23 23:26:55 +0700

    Improved logo and text to Craig's suggestion

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -7,7 +7,7 @@ class Footer extends HTMLElement {
     this.innerHTML = `
         <style>
             .footerlogo {
-                height: 50px;
+                height: 40px;
             }
 
             .grap-vert {
@@ -74,7 +74,7 @@ class Footer extends HTMLElement {
 
             @media (min-width: 768px) {
                 .footerlogo {
-                    height: 60px;
+                    height: 40px;
                 }
 
                 .footer-nav-link {
@@ -117,7 +117,7 @@ class Footer extends HTMLElement {
                         <div class="col-md-12 col-lg-3 order-lg-3">
                             <div class="row logogroup">
                                 <div class="col-md-6 col-lg-12">
-                                    <img class="footerlogo" style="margin-left: -12px" src="assets/img/logo.svg" alt="BrownSim logo" />
+                                    <img class="footerlogo" style="margin-left: -10px" src="assets/img/logo.svg" alt="BrownSim logo" />
                                 </div>
                                 <div class="col-md-6 col-lg-12">
                                     <img class="footerlogo" src="assets/img/procue-logo.svg" alt="ProCue logo" />
